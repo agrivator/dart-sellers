c19ec0f079e187561a582917097aa6db7e440716
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-04-27 19:32:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dart Sellers',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
